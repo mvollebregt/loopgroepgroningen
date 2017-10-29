6dd86588bfb8f4bd7e7206ea17d22f3a27e90e3f
Michel Vollebregt <mi.vollebregt@gmail.com>
2017-10-29 11:53:15 +0100

    Auto resizing text field + styling

--- src/pages/prikbord/prikbord.ts
+++ src/pages/prikbord/prikbord.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {Content, IonicPage} from 'ionic-angular';
+import {Content, IonicPage, TextInput} from 'ionic-angular';
 import {Bericht} from "../../core/bericht";
 import {Observable} from "rxjs/Observable";
 import {PrikbordService} from "../../core/prikbord.service";
@@ -16,6 +16,7 @@ export class PrikbordPage implements OnInit {
   items: Observable<Bericht[]>;
 
   private initialized = false;
+  private contentHeight = -1;
 
   constructor(private prikbordService: PrikbordService) {
   }
@@ -26,6 +27,28 @@ export class PrikbordPage implements OnInit {
       setTimeout(() => {
         this.content.scrollToBottom(this.initialized ? 300 : 0);
         this.initialized = values.length > 0;
+      });
+    });
+  }
+
+  resize(input: TextInput): void {
+    setTimeout(() => {
+      let textArea = input.getNativeElement().querySelector('textArea');
+      textArea.style.overflow = "hidden";
+      textArea.style.height = "auto";
+      textArea.style.height = Math.min(textArea.scrollHeight, 165) + "px";
+      if (this.contentHeight === -1) {
+        this.contentHeight = this.content.contentHeight;
+      }
+      this.content.resize();
+      let scrollTopBefore = this.content.getContentDimensions().scrollTop;
+      setTimeout(() => {
+        let newHeight = this.content.contentHeight;
+        let scrollTop = this.content.getContentDimensions().scrollTop;
+        if (scrollTop === scrollTopBefore) {
+          this.content.scrollTo(0, scrollTop - newHeight + this.contentHeight, 0);
+        }
+        this.contentHeight = newHeight;
       });
     });
   }
