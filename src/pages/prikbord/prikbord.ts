3239918d13ba670a585bdc8efe120da47b299700
Michel Vollebregt <mi.vollebregt@gmail.com>
2017-12-06 16:09:25 +0100

    Fix voor layout-probleem?

--- src/pages/prikbord/prikbord.ts
+++ src/pages/prikbord/prikbord.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Content, IonicPage, TextInput} from 'ionic-angular';
 import {Bericht} from "../../core/bericht";
 import {Observable} from "rxjs/Observable";
@@ -11,14 +11,16 @@ import {PrikbordService} from "../../core/prikbord.service";
 })
 export class PrikbordPage implements OnInit {
 
-  @ViewChild(Content) private content: Content;
-
   items: Observable<Bericht[]>;
+  editing = false;
+
+  @ViewChild(Content) private content: Content;
+  @ViewChild('textarea') private textInput: TextInput;
 
   private initialized = false;
   private contentHeight = -1;
 
-  constructor(private prikbordService: PrikbordService) {
+  constructor(private prikbordService: PrikbordService, private changeDetector: ChangeDetectorRef) {
   }
 
   ngOnInit() {
@@ -31,25 +33,38 @@ export class PrikbordPage implements OnInit {
     });
   }
 
+  startEditing() {
+    this.editing = true;
+    this.content.resize();
+    this.changeDetector.detectChanges();
+    this.textInput.setFocus();
+  }
+
+  stopEditing() {
+    this.editing = false;
+    this.content.resize();
+    setTimeout(() => this.content.scrollToBottom(0));
+  }
+
   resize(input: TextInput): void {
-    setTimeout(() => {
-      let textArea = input.getNativeElement().querySelector('textArea');
-      textArea.style.overflow = "hidden";
-      textArea.style.height = "auto";
-      textArea.style.height = Math.min(textArea.scrollHeight, 165) + "px";
-      if (this.contentHeight === -1) {
-        this.contentHeight = this.content.contentHeight;
-      }
-      this.content.resize();
-      let scrollTopBefore = this.content.getContentDimensions().scrollTop;
-      setTimeout(() => {
-        let newHeight = this.content.contentHeight;
-        let scrollTop = this.content.getContentDimensions().scrollTop;
-        if (scrollTop === scrollTopBefore) {
-          this.content.scrollTo(0, scrollTop - newHeight + this.contentHeight, 0);
-        }
-        this.contentHeight = newHeight;
-      });
-    });
+    // setTimeout(() => {
+    //   let textArea = input.getNativeElement().querySelector('textInput');
+    //   textArea.style.overflow = "hidden";
+    //   textArea.style.height = "auto";
+    //   textArea.style.height = Math.min(textArea.scrollHeight, 165) + "px";
+    //   if (this.contentHeight === -1) {
+    //     this.contentHeight = this.content.contentHeight;
+    //   }
+    //   this.content.resize();
+    //   let scrollTopBefore = this.content.getContentDimensions().scrollTop;
+    //   setTimeout(() => {
+    //     let newHeight = this.content.contentHeight;
+    //     let scrollTop = this.content.getContentDimensions().scrollTop;
+    //     if (scrollTop === scrollTopBefore) {
+    //       this.content.scrollTo(0, scrollTop - newHeight + this.contentHeight, 0);
+    //     }
+    //     this.contentHeight = newHeight;
+    //   });
+    // });
   }
 }
