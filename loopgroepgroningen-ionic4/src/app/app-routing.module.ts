666afb20ad67436736bc4b0df41414d3c703bfa9
Michel Vollebregt <mi.vollebregt@gmail.com>
2018-07-30 11:29:59 +0200

    #16 proberen van nieuwe router-based navigatie

--- loopgroepgroningen-ionic4/src/app/app-routing.module.ts
+++ loopgroepgroningen-ionic4/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'list/:id',
+    loadChildren: './list/list.module#ListPageModule'
   }
 ];
 
--- loopgroepgroningen-ionic4/src/app/list/list.page.ts
+++ loopgroepgroningen-ionic4/src/app/list/list.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {pluck} from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +8,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
+
+  selectedItem: any;
+
   private icons = [
     'flask',
     'wifi',
@@ -19,11 +23,12 @@ export class ListPage implements OnInit {
     'bluetooth',
     'build'
   ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public items: Array<{ id: number, title: string; note: string; icon: string }> = [];
 
-  constructor() {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     for (let i = 1; i < 11; i++) {
       this.items.push({
+        id: (i - 1),
         title: 'Item ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
@@ -32,10 +37,17 @@ export class ListPage implements OnInit {
   }
 
   ngOnInit() {
+    this.activatedRoute.params.pipe(
+      pluck('id')
+    ).subscribe((id: number) => {
+      console.log(id);
+      this.selectedItem = this.items[id];
+      console.log(this.selectedItem);
+    });
   }
 
   // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+  navigate(item) {
+    this.router.navigate(['/list', item.id]);
+  }
 }
