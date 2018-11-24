60860abbac20943a11dd9c64a68d44c4cc9f8423
Michel Vollebregt <mi.vollebregt@gmail.com>
2018-11-24 11:09:45 +0100

    Styling verbeteren + code opruimen

--- loopgroepgroningen-ionic4/src/app/shared/backend/url-resolver.service.ts
+++ loopgroepgroningen-ionic4/src/app/shared/backend/url-resolver.service.ts
@@ -1,16 +1,13 @@
 import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class UrlResolverService {
-
-  private readonly baseUrl = 'http://localhost:5000/loopgroep-groningen-v3/us-central1';
-
-  // private readonly baseUrl = 'https://us-central1-loopgroep-groningen-v3.cloudfunctions.net';
 
   urlFor(relativeUrl: string): string {
     const serverNameIndex = relativeUrl.indexOf('loopgroepgroningen.nl/');
     const normalizedUrl = serverNameIndex === -1 ? relativeUrl : relativeUrl.substring(serverNameIndex + 'loopgroepgroningen.nl/'.length);
     const separator = normalizedUrl.startsWith('/') ? '' : '/';
-    return this.baseUrl + separator + normalizedUrl;
+    return environment.api + separator + normalizedUrl;
   }
 }
--- loopgroepgroningen-ionic4/src/environments/environment.prod.ts
+++ loopgroepgroningen-ionic4/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://us-central1-loopgroep-groningen-v3.cloudfunctions.net'
 };
