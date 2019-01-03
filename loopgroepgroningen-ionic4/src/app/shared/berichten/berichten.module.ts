d91b5a3d48d896fd6f26016c97d9098f685846e3
Michel Vollebregt <mi.vollebregt@gmail.com>
2019-01-03 10:35:56 +0100

    Reactiecomponent toegevoegd

--- loopgroepgroningen-ionic4/src/app/shared/berichten/berichten.module.ts
+++ loopgroepgroningen-ionic4/src/app/shared/berichten/berichten.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {GesprekComponent} from './gesprek/gesprek.component';
 import {SharedModule} from '../shared/shared.module';
 import {RichContentModule} from '../rich-content/rich-content.module';
+import {ReactieComponent} from './reactie/reactie.component';
 
-const directives = [GesprekComponent];
+const directives = [GesprekComponent, ReactieComponent];
 
 @NgModule({
   imports: [
---
+++ loopgroepgroningen-ionic4/src/app/shared/berichten/reactie/reactie.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'lg-reactie',
+  templateUrl: 'reactie.component.html',
+  styleUrls: ['reactie.component.scss']
+})
+export class ReactieComponent {
+
+}
